da0946aa9b3218e7e36fdb09c15feb907ee24436
Igor Šarčević <igor@renderedtext.com>
2017-11-24 23:42:30 +0000

    Passing tests for search function

--- src/lib.rs
+++ src/lib.rs
@@ -27,5 +27,37 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     file.read_to_string(&mut content)
         .expect("Something went wrong while reading the file");
 
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
